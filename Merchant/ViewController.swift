793f73c6a0e25c6f35bfadff1bd4b4fb739f02ff
Luke Li <luke.li@shoalter.com>
2022-01-18 13:45:54 +0800

    Initial Commit

---
+++ Merchant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Merchant
+//
+//  Created by Luke Lee on 2022/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
